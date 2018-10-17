898c97ea4068a077e4f7467ecc3ae3f42d567c4f
Guest User <Guest@Alihans-MacBook-Pro.local>
2018-10-17 13:32:28 +0300

    Initial Commit

---
+++ Getaround Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Getaround Challenge
+//
+//  Created by Guest User on 17.10.2018.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Getaround ChallengeTests/Getaround_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Getaround_ChallengeTests.swift
+//  Getaround ChallengeTests
+//
+//  Created by Guest User on 17.10.2018.
+//  Copyright © 2018 Guest User. All rights reserved.
+//
+
+import XCTest
+@testable import Getaround_Challenge
+
+class Getaround_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
